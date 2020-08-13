a9eb219862748934942d9cbc6dbba21494cb173a
daydaygo <1252409767@qq.com>
2020-08-13 17:57:23 +0800

    feat: set `edit on github` url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,16 +39,21 @@ module.exports = {
         {
           text: '了解更多',
           items: [
+              { text: '微服务治理框架', link: 'https://github.com/douyu/jupiter' },
               { text: '微服务管理平台', link: 'https://github.com/douyu/juno' },
           ]
         },
+        {
+            text: 'GitHub',
+            link: 'https://github.com/douyu/jupiter'
+      },
 
       ],
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/douyu/jupiter',
+        repo: 'douyu/jupiter-website',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        // repoLabel: '查看源码',
+        repoLabel: '查看文档源码',
         // 假如文档不是放在仓库的根目录下：
         docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
